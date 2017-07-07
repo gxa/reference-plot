04b69c75803a860384d91e921b7c979d349896ac
barrera <barrera@ebi.ac.uk>
2017-07-07 15:17:55 +0100

    Changed path to get ./TSNEPlotContainer.jsx in index.js and removed unused renderer

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import TSNEPlotContainer from './TSNEPlotContainer.jsx';
+import TSNEPlotContainer from '../src/TSNEPlotContainer.jsx';
 
 const render = function (options, container) {
     ReactDOM.render(<TSNEPlotContainer {...options} />, document.getElementById(container))
