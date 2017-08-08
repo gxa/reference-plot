697d8aefb32af1a241927979b159e6e12673348b
barrera <barrera@ebi.ac.uk>
2017-08-08 08:24:54 +0100

    Change method for highlighting the cluster and use color 2.0

--- src/ScatterPlot.js
+++ src/ScatterPlot.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import Color from 'color'
+
 import ReactHighcharts from 'react-highcharts'
 import shallowCompare from "react-addons-shallow-compare";
 
@@ -62,12 +64,28 @@ class ScatterPlot extends React.Component {
     }
 
     highlightCluster(clusterId) {
-        for (let cluster of clusterId) {
-            const c = this.refs.chart.chart.series[cluster].data;
-            for (let i = 0; i < c.length; i++) {
-                this.refs.chart.chart.series[cluster].data[i].setState('hover');
-            }
+        // for (let cluster of clusterId) {
+        //     const c = this.refs.chart.chart.series[cluster].data;
+        //     for (let i = 0; i < c.length; i++) {
+        //         this.refs.chart.chart.series[cluster].data[i].setState('hover');
+        //     }
+        // }
+
+        if (clusterId.length) {
+            this.refs.chart.chart.series.forEach((series, seriesIndex) => {
+                const c = this.refs.chart.chart.series[seriesIndex].data;
+                for (let i = 0; i < c.length; i++) {
+                    if (clusterId.includes(seriesIndex)) {
+                        // this.refs.chart.chart.series[seriesIndex].data[i].setState('hover');
+                    } else {
+                        const thisPointColor = Color(this.refs.chart.chart.series[seriesIndex].data[i].graphic.attr(`fill`))
+                        // this.refs.chart.chart.series[seriesIndex].data[i].graphic.attr({fill: thisPointColor.desaturate(0.65).lighten(0.25).rgb().string()})
+                        this.refs.chart.chart.series[seriesIndex].data[i].graphic.attr({fill: thisPointColor.whiten(0.15).desaturate(0.50).lighten(0.25).rgb().string()})
+                    }
+                }
+            })
         }
+
     }
 
     componentWillReceiveProps(nextProps) {
